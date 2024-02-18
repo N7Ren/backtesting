c63f67e31d5f1f64445b82e4c7a72a5c15c2b939
N7Ren <nsevenren@gmail.com>
2024-02-18 12:16:49 +0100

    added rsi log

--- strategies/rsi.py
+++ strategies/rsi.py
@@ -1,13 +1,33 @@
 import backtrader as bt
 
 class RSI(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+
     def __init__(self):
         self.rsi = bt.indicators.RSI(period=14)  # Calculate RSI with period 14
 
     def next(self):
+
         if self.rsi < 30:  # Buy when RSI goes below 30
-            self.close()
+            if self.position:
+                self.log('POSITION BEFORE BUY CLOSE, %.2f' % self.position.size)
+                self.close()
+                self.log('POSITION AFTER BUY CLOSE, %.2f' % self.position.size)
+
+            self.log('POSITION BEFORE BUY CREATE, %.2f' % self.position.size)
             self.buy()
+            self.log('POSITION BEFORE BUY CREATE, %.2f' % self.position.size)
         elif self.rsi > 70:  # Sell when RSI goes above 70
-            self.close()
-            #self.sell()
+            if self.position:
+                self.log('POSITION BEFORE SELL CLOSE, %.2f' % self.position.size)
+                self.close()
+                self.log('POSITION AFTER SELL CLOSE, %.2f' % self.position.size)
+
+            self.log('POSITION BEFORE SELL CREATE, %.2f' % self.position.size)
+            self.sell()
+            self.log('POSITION AFTER SELL CREATE, %.2f' % self.position.size)
